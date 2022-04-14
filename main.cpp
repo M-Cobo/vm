b5c1b096a1c8c3bd8aa114002b97ed719324db18
Manuel Cobo <yahilcobo@hotmail.com>
2022-04-14 16:16:35 -0400

    LC-3 Architecture.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,64 @@
-#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+#include <signal.h>
+/* unix */
+#include <unistd.h>
+#include <fcntl.h>
+
+#include <sys/time.h>
+#include <sys/types.h>
+#include <sys/termios.h>
+#include <sys/mman.h>
+
+enum
+{
+    R_R0 = 0,
+    R_R1,
+    R_R2,
+    R_R3,
+    R_R4,
+    R_R5,
+    R_R6,
+    R_R7,
+    R_PC, /* program counter*/
+    R_COND,
+    R_COUNT,
+};
+
+enum
+{
+    FL_POS = 1 << 0, /* P */
+    FL_ZRO = 1 << 1, /* Z */
+    FL_NEG = 1 << 2, /* N */
+};
+
+enum
+{
+    OP_BR = 0, /* branch */
+    OP_ADD, /* add */
+    OP_LD, /* load */
+    OP_ST, /* store */
+    OP_JSR, /* jump register */
+    OP_AND, /* bitwise and */
+    OP_LDR, /* load register */
+    OP_STR, /* store register */
+    OP_RTI, /* unused */
+    OP_NOT, /* bitwise not */
+    OP_LDI, /* load indirect */
+    OP_STI, /* store indirect */
+    OP_JMP, /* jump */
+    OP_RES, /* reserve (unused) */
+    OP_LEA, /* load effective address */
+    OP_TRAP, /* execute trap */
+};
+
+#define MEMORY_MAX (1 << 16)
+uint16_t memory[MEMORY_MAX]; /* 65536 locations */
+uint16_t reg[R_COUNT];
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
     return 0;
 }
