ecf5b66c95af3e25e02c77d4283fd3af183aa7c5
Manuel Cobo <yahilcobo@hotmail.com>
2022-04-15 14:45:36 -0400

    Added instructions helpers.

--- main.cpp
+++ main.cpp
@@ -58,6 +58,31 @@ enum
 uint16_t memory[MEMORY_MAX]; /* 65536 locations */
 uint16_t reg[R_COUNT];
 
+uint16_t sign_extend(uint16_t x, int bit_count)
+{
+    if ((x >> (bit_count - 1)) & 1)
+    {
+        x |= (0xffff << bit_count);
+    }
+    return x;
+}
+
+void update_flags(uint16_t r)
+{
+    if (reg[r] == 0)
+    {
+        reg[R_COND] = FL_ZRO;
+    }
+    else if (reg[r] << 15) /* a 1 in the left-most bit indicates negative */
+    {
+        reg[R_COND] = FL_NEG;
+    }
+    else
+    {
+        reg[R_COND] = FL_POS;
+    }
+}
+
 int main() {
 
     return 0;
